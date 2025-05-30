3dd9b43fe84b53d0896df1fe056a369a96cb37b3
Le Van Hoang Tan <164925430+ayaflo@users.noreply.github.com>
2025-05-30 22:17:11 +0700

    Add flutter_cooking_app/lib/screens/profile_screen.dart 1748618224657

---
+++ flutter_cooking_app/lib/screens/profile_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Center(
+        child: Text(
+          'User Profile Screen',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
